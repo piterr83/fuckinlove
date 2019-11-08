2e85bd5acbbd8aa92134e2c6754fbf03c956ec15
piterr83 <playmakerpit@poczta.onet.pl>
2019-11-08 21:17:37 +0100

    Updates

---
+++ precache-manifest.1b726726210808c0967dfcda0b63d2f3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6eb2e8e59939ba3cee3e1d3457304d1",
+    "url": "/fuckinlove/index.html"
+  },
+  {
+    "revision": "c1bdc01a521eee1422dd",
+    "url": "/fuckinlove/static/css/main.6d6c9850.chunk.css"
+  },
+  {
+    "revision": "4f67474faad15eb944c0",
+    "url": "/fuckinlove/static/js/2.b7708659.chunk.js"
+  },
+  {
+    "revision": "c1bdc01a521eee1422dd",
+    "url": "/fuckinlove/static/js/main.bcd16223.chunk.js"
+  },
+  {
+    "revision": "8a1c67ba7ca062249053",
+    "url": "/fuckinlove/static/js/runtime-main.bd0adc84.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/fuckinlove/static/media/logo.25bf045c.svg"
+  }
+]);

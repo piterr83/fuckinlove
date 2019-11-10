03b01d18924ea4fe63c71f499c9ce6f9e7657b3f
Piotr Lisowski <plisowski@um.lan>
2019-11-10 14:09:46 +0100

    Updates

---
+++ precache-manifest.848241fa0320456fd77cd1d01b425cb2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a7ba1e540b45886e8df212e56984830",
+    "url": "/fuckinlove/index.html"
+  },
+  {
+    "revision": "523851622130f4c5ed6e",
+    "url": "/fuckinlove/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "8ba2aeac34dfe8023a51",
+    "url": "/fuckinlove/static/js/2.df8cecb2.chunk.js"
+  },
+  {
+    "revision": "523851622130f4c5ed6e",
+    "url": "/fuckinlove/static/js/main.951c1de5.chunk.js"
+  },
+  {
+    "revision": "ab121121c4013bfd391f",
+    "url": "/fuckinlove/static/js/runtime-main.d1ed5336.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/fuckinlove/static/media/logo.25bf045c.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/fuckinlove/precache-manifest.af51363bfa489a317b0b4da6f8f6778f.js"
+  "/fuckinlove/precache-manifest.848241fa0320456fd77cd1d01b425cb2.js"
 );
 
 self.addEventListener('message', (event) => {

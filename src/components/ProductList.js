9881916660d796fad89e240ad98740963031b97f
piterr83 <playmakerpit@poczta.onet.pl>
2020-01-07 22:58:17 +0100

    first commit

---
+++ src/components/ProductList.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react'
+import Product from "./Product.js"
+import Title from "./Title.js"
+import {storeProducts} from '../data.js'
+import {ProductConsumer}  from '../context.js'
+
+export default class ProductList extends Component {
+   state ={
+       products:storeProducts
+   }
+    render() {
+        return (
+            <React.Fragment>
+
+<div className="py-5">
+    <div className="container">
+        <Title name="our" title="products" />
+        <div className="row">
+<ProductConsumer>
+    {
+        value=>{
+return value.products.map(product =>{
+    return <Product key={product.id} product={product} />
+})
+        }
+    }
+</ProductConsumer>
+        </div>
+    </div>
+</div>
+
+            </React.Fragment>
+        )
+    }
+}
